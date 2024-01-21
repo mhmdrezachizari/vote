58b738541aed850ce9105d23996ef00d93a8c069
mohammadrezachizari <mhmdrezachizari@gmail.com>
2024-01-21 19:23:09 +0330

    Upload simple PWA project on Github

---
+++ vote.py
@@ -0,0 +1,14 @@
+v0=0
+v1=0
+for i in range(25):
+    a=int(input())
+    if a==1:
+        v1=v1+1
+    elif a==0:
+        v0=v0+1
+    else:
+        continue
+print(v1)
+print(v0)
+# soal mohasebe ray haye 2 kandid
+#ray ha ya 0 ya 1 hast 
